49b80a2a86edb5f57d3e9db9136f256d41d030ef
Dima Katz <DimaK@finonex.com>
2024-06-16 21:16:14 +0300

    Fix

---
+++ ProductsManagment.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using ProductsManagment.API.Controllers;
+using ProductsManagment.BLL.Services;
+using Xunit;
+
+namespace ProductsManagment.Test
+{
+    public class UnitTest1
+    {
+        IProductService productService;
+        [Fact]
+        public void Test1()
+        {
+            productService = new Mock
+           var controller = new ProductsController()
+        }
+    }
+}
